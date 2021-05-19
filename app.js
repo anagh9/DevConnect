d7c5c22bcf5f19f305d9a2d5c0398bda15af77ba
ANAGH <anagh9931@gmail.com>
2021-05-19 22:44:38 +0530

    Users Validation

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
 const PORT = process.env.PORT || 5000;
 
 //Connect Database
 connectDB();
+
+// Init Middleware
+app.use(express.json({extended:false}));
+
 app.get("/", (req, res) => res.send("API Running"));
 
 // Define Routes
